ef759ef1672096d78ab4ed9da4c6420f0356a540
Amelia Wijaya <126996983+Amel-ia@users.noreply.github.com>
2023-06-09 12:39:03 +0700

    Add files via upload

---
+++ est_mobill.py
@@ -0,0 +1,23 @@
+import pickle
+import streamlit as st
+
+model = pickle.load(open('estimasi_mobill.sav', 'rb'))
+
+st.title('Estimasi Harga Mobil Bekas dengan linear regression')
+
+year = st.number_input('Input Tahun Mobil')
+mileage = st.number_input('Input KM Mobil')
+tax = st.number_input('Input Pajak Mobil')
+mpg = st.number_input('Input konsumsi BBM Mobil')
+engineSize = st.number_input('Input Engine Size')
+
+predict = ''
+
+if st.button('Estimasi Harga'):
+    predict = model.predict(
+        [[year,mileage, tax, mpg, engineSize]]
+    )
+    st.write ('Estimasi Harga Mobil Bekas dalam EUR : ', predict)
+    st.write ('Estimasi Harga Mobil Bekas dalam IDR (Juta) : ', predict*16500)
+    
+    
